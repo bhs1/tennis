95265028db5227860300b70f568a72f685428404
bhs1 <bensc77@gmail.com>
2023-04-23 23:07:03 +0000

    crontab is live! Checking times for you for Sunday

--- tennis.sh
+++ tennis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source venv/bin/activate
-python3 tennis.py --input_date 04/27/2023 --input_interval 60 --input_time_range 7:00am 9:00am --api_token "$1" --user_token "$2" --debug
+source ~/tennis/venv/bin/activate
+python3 ~/tennis/tennis.py --input_date 04/30/2023 --input_interval 60 --input_time_range 10:00am 8:00pm --api_token "$1" --user_token "$2" #--debug
