5a02e9de3c14ccc9053dfdf33a6352238839e964
Ben <bensc77@gmail.com>
2023-04-23 18:07:55 -0400

    Notification works with real times!

--- tennis.py
+++ tennis.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import argparse
+import json
+import http.client, urllib
 
 ###################### START CONSTANTS ####################
 
 cookies = {
-    'PHPSESSID': '3q6gjdrceq2pp6geqrnpfkeh6b',
+    'PHPSESSID': 'dctuhjqn7fbliji6blihqgmbn9',
     'SessionExpirationTime': '1681388464',
     'isLoggedIn': '1',
 }
@@ -22,7 +26,6 @@ headers = {
     'X-INSTANA-L': '1,correlationType=web;correlationId=f2983719aaf08d0e',
     'Origin': 'https://gtc.clubautomation.com',
     'Connection': 'keep-alive',
-    # 'Cookie': 'PHPSESSID=3q6gjdrceq2pp6geqrnpfkeh6b; SessionExpirationTime=1681388464; isLoggedIn=1',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -72,17 +75,60 @@ def get_raw_response(date, interval):
     )
     return response
 
-########################## FUNCTIONS ###############################
+def filter_activities_by_time(activities, input_time_range):
+    filtered_activities = {}
+    for activity, times in activities.items():
+        filtered_times = []
+        for time_str in times:
+            time_obj = datetime.strptime(time_str, '%I:%M%p')
+            if input_time_range[0].lower() <= time_str.lower() <= input_time_range[1].lower():
+                filtered_times.append(time_str)
+        if filtered_times:
+            filtered_activities[activity] = filtered_times
+    return filtered_activities
+
+def send_notification(title, body, url, pb_token):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+    urllib.parse.urlencode({
+        "token": "asg21wetbwa33qgk8vfhm7dodat6vp",
+        "user": "uc1dk4b66zk4kpbztp8dk1wzow6j2r",
+        "message": body,
+        "url": url,
+        "title": title,
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+
+########################## FUNCTIONS ##############################
+
 
 # Parse inputs
+parser = argparse.ArgumentParser(description='Accept input variables')
 
-input_date = '04/13/2023'
-input_interval = '60'
-input_time_range = ['4:00pm', '7:00pm']
+parser.add_argument('--input_date', type=str, help='Input date in mm/dd/yyyy format')
+parser.add_argument('--input_interval', type=int, help='Input interval in minutes')
+parser.add_argument('--input_time_range', nargs='+', type=str, help='Input time range as a list of start and end times in hh:mm[am/pm] format')
+parser.add_argument('--pb_token', help="Your pushbot token")
+parser.add_argument('--debug', action='store_true', help='Enable debug mode')
 
+args = parser.parse_args()
 
+input_date = args.input_date
+input_interval = args.input_interval
+input_time_range = args.input_time_range
+pb_token=args.pb_token
+debug = args.debug
+
+# Get response
 response = get_raw_response(input_date, input_interval)
+
+if debug:
+    print("Raw response:\n"  + response.text)
+
 soup = BeautifulSoup(response.text, 'html.parser')
+
+if 'First time here' in response.text:
+    print("ERROR: Got login page. Try replacing PHP Session ID with a fresh one!")
 
 activities = {}
 
@@ -93,6 +139,10 @@ for td in soup.find_all('td'):
         times.append(a.text.strip())
     activities[activity] = times
 
-# Next: Filter times by input time range
+filtered_activities = filter_activities_by_time(activities, input_time_range)
 
-print(activities)
+# If filtered acttivities is not empty, send a pushbullet notification (with info + link)
+if len(filtered_activities) > 0:
+    send_notification("Activities available!", filtered_activities, 'https://gtc.clubautomation.com', pb_token)
+
+print(filtered_activities)
