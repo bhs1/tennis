0ab5958a792b08840a8a5ea12ea3fe79ae46e480
bhs1 <bensc77@gmail.com>
2023-09-17 22:29:35 -0700

    Add readme, update tennis.sh and update requirements

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+python -m venv tennis_env
+source tennis_env/bin/activate
+pip install -r requirements.txt
+chmod +x tennis.sh
+./tennis.sh
+```
--- tennis.sh
+++ tennis.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source ~/Documents/tennis/venv/bin/activate
-python3 ~/Documents/tennis/tennis.py --input_date 04/30/2023 --input_interval 60 --input_time_range 10:00am 8:00pm --api_token "$1" --user_token "$2" --activity_filter "Pickleball / Mini Tennis" #--debug
+python3 tennis.py --input_date 04/30/2023 --input_interval 60 --input_time_range 10:00am 8:00pm --api_token "$1" --user_token "$2" --activity_filter "Pickleball / Mini Tennis" #--debug
